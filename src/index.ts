3908df2202376059a6b623ae3d74cee5c9c0c096
Ugui10 <guilhermenied01@gmail.com>
2023-10-02 20:33:14 -0300

    Adding tsx

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen();
+app.listen(port, () => {
+  console.log(`Running on port ${port}🚀`);
+});
