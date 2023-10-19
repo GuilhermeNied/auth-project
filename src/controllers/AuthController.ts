a57c14cb91b4e755fd3ee9e1c03aaf38359c0b9a
Guilherme <guilhermenied01@gmail.com>
2023-10-19 07:43:22 -0300

    More

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,9 @@
 import { Response, Request } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export class AuthController {
-  // constructor () {}
-
   register(req: Request, res: Response): Response {
     const { name, username, password } = req.body;
 
@@ -16,64 +16,70 @@ export class AuthController {
 
       return res.status(201).json(user);
     } catch (error) {
-      return res.send(500).json({ message: error });
+      return res.status(500).json({ message: error });
     }
   }
 
   login(req: Request, res: Response): Response {
     const { username, password } = req.body;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const jwtSecretKey: string = process.env.JWT_SECRET_KEY!;
 
     try {
-      const pass = '1234';
+      // const pass = '1234';
+      const userId = 312312312;
 
-      const hashedPassword = bcrypt.hashSync(pass, 10);
+      // const hashedPassword = bcrypt.hashSync(pass, 10);
 
-      const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
+      // const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
 
-      if (!isPasswordValid) {
-        return res.sendStatus(400);
-      }
+      // if (!isPasswordValid) {
+      //   return res.sendStatus(400);
+      // }
+      const token = jwt.sign({ id: userId }, jwtSecretKey);
 
-      return res.sendStatus(200);
-    } catch (error) {
-      return res.send(500).json({ message: error });
-    }
-  }
+      const user = { token, userId };
 
-  getAll(req: Request, res: Response): Response {
-    const users = [
-      { name: 'John', username: 'John' },
-      { name: 'Gui', username: 'Gui' },
-    ];
-    return res.status(200).json(users);
-  }
-
-  get(req: Request, res: Response): Response {
-    const { username } = req.params;
-    const users = [
-      { name: 'John', username: 'John' },
-      { name: 'Gui', username: 'Gui' },
-    ];
-    const user = users.find((user) => user.username === username);
-    return res.status(200).json(user);
-  }
-
-  update(req: Request, res: Response): Response {
-    const users = [
-      { name: 'John', username: 'John' },
-      { name: 'Gui', username: 'Gui' },
-    ];
-    const { name } = req.body;
-    const { username } = req.params;
-    const user = users.find((user) => user.username === username);
-    if (name && user) {
-      user.name = name;
       return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json({ message: error });
     }
-    return res.sendStatus(400);
   }
 
-  delete(req: Request, res: Response): Response {
-    return res.sendStatus(204);
-  }
+  // getAll(req: Request, res: Response): Response {
+  //   const users = [
+  //     { name: 'John', username: 'John' },
+  //     { name: 'Gui', username: 'Gui' },
+  //   ];
+  //   return res.status(200).json(users);
+  // }
+
+  // get(req: Request, res: Response): Response {
+  //   const { username } = req.params;
+  //   const users = [
+  //     { name: 'John', username: 'John' },
+  //     { name: 'Gui', username: 'Gui' },
+  //   ];
+  //   const user = users.find((user) => user.username === username);
+  //   return res.status(200).json(user);
+  // }
+
+  // update(req: Request, res: Response): Response {
+  //   const users = [
+  //     { name: 'John', username: 'John' },
+  //     { name: 'Gui', username: 'Gui' },
+  //   ];
+  //   const { name } = req.body;
+  //   const { username } = req.params;
+  //   const user = users.find((user) => user.username === username);
+  //   if (name && user) {
+  //     user.name = name;
+  //     return res.status(200).json(user);
+  //   }
+  //   return res.sendStatus(400);
+  // }
+
+  // delete(req: Request, res: Response): Response {
+  //   return res.sendStatus(204);
+  // }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,13 +4,13 @@ import { AuthController } from '../controllers/AuthController';
 export const router: Router = Router();
 const authController = new AuthController();
 
-router.get('/:username', (req: Request, res: Response) => {
-  authController.get(req, res);
-});
+// router.get('/:username', (req: Request, res: Response) => {
+//   authController.get(req, res);
+// });
 
-router.get('/users', (req: Request, res: Response) => {
-  authController.getAll(req, res);
-});
+// router.get('/users', (req: Request, res: Response) => {
+//   authController.getAll(req, res);
+// });
 
 router.post('/register', (req: Request, res: Response) => {
   authController.register(req, res);
@@ -20,10 +20,10 @@ router.post('/login', (req: Request, res: Response) => {
   authController.login(req, res);
 });
 
-router.patch('/:username', (req: Request, res: Response) => {
-  authController.update(req, res);
-});
+// router.patch('/:username', (req: Request, res: Response) => {
+//   authController.update(req, res);
+// });
 
-router.delete('/:username', (req: Request, res: Response) => {
-  authController.delete(req, res);
-});
+// router.delete('/:username', (req: Request, res: Response) => {
+//   authController.delete(req, res);
+// });
