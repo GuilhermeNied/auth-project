508648c93760e5c92ad81be6de442338634c8b3c
Guilherme <guilhermenied01@gmail.com>
2023-10-19 07:29:04 -0300

    Adding bcrypt

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,20 +1,47 @@
-import { Response, type Request } from 'express';
+import { Response, Request } from 'express';
+import bcrypt from 'bcrypt';
 
 export class AuthController {
   // constructor () {}
 
   register(req: Request, res: Response): Response {
     const { name, username, password } = req.body;
-    // const hasedPassword =
-    const user = { name, username, password };
-    // const
-    return res.status(201).json(user);
+
+    try {
+      const hashedPassword = bcrypt.hashSync(password, 10);
+      console.log(hashedPassword);
+
+      const user = { name, username, hashedPassword };
+      console.log(user);
+
+      return res.status(201).json(user);
+    } catch (error) {
+      return res.send(500).json({ message: error });
+    }
   }
 
   login(req: Request, res: Response): Response {
     const { username, password } = req.body;
 
-    return res.sendStatus(200);
+    try {
+      const pass = '1234';
+      console.log(pass);
+      console.log(password);
+
+      const hashedPassword = bcrypt.hashSync(pass, 10);
+      console.log(hashedPassword);
+
+      const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
+      console.log(isPasswordValid);
+
+      if (!isPasswordValid) {
+        return res.sendStatus(400);
+      }
+
+      return res.sendStatus(200);
+    } catch (error) {
+      return res.send(500).json({ message: error });
+    }
   }
 
   getAll(req: Request, res: Response): Response {
