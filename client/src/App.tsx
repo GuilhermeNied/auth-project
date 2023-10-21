480d6bad3fd4da914d2de97fdef55c021e1f69b0
Guilherme <guilhermenied01@gmail.com>
2023-10-21 17:23:28 -0300

    Creating home page and adding routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
-function App() {
-  return (
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { ColorSide } from './components/ColorSide'
+import './App.css'
 
-    <div>Hello</div>
+export function App() {
+  return (
+    <div className="app-container">
+      <BrowserRouter>
+        <ColorSide />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
   )
 }
-
-export default App
---
+++ client/src/components/ColorSide/index.tsx
@@ -0,0 +1,5 @@
+import './styles.css'
+
+export function ColorSide() {
+  return <div className='color-side-container' />
+}
