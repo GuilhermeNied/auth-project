e74c0ec18a22904206dc98078f05b5cc5b1b3979
Guilherme <guilhermenied01@gmail.com>
2023-10-21 11:41:03 -0300

    Removing unnecessary routes and controllers

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -41,41 +41,4 @@ export class AuthController {
       return res.status(500).json({ message: error });
     }
   }
-
-  // getAll(req: Request, res: Response): Response {
-  //   const users = [
-  //     { name: 'John', username: 'John' },
-  //     { name: 'Gui', username: 'Gui' },
-  //   ];
-  //   return res.status(200).json(users);
-  // }
-
-  // get(req: Request, res: Response): Response {
-  //   const { username } = req.params;
-  //   const users = [
-  //     { name: 'John', username: 'John' },
-  //     { name: 'Gui', username: 'Gui' },
-  //   ];
-  //   const user = users.find((user) => user.username === username);
-  //   return res.status(200).json(user);
-  // }
-
-  // update(req: Request, res: Response): Response {
-  //   const users = [
-  //     { name: 'John', username: 'John' },
-  //     { name: 'Gui', username: 'Gui' },
-  //   ];
-  //   const { name } = req.body;
-  //   const { username } = req.params;
-  //   const user = users.find((user) => user.username === username);
-  //   if (name && user) {
-  //     user.name = name;
-  //     return res.status(200).json(user);
-  //   }
-  //   return res.sendStatus(400);
-  // }
-
-  // delete(req: Request, res: Response): Response {
-  //   return res.sendStatus(204);
-  // }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,14 +4,6 @@ import { AuthController } from '../controllers/AuthController';
 export const router: Router = Router();
 const authController = new AuthController();
 
-// router.get('/:username', (req: Request, res: Response) => {
-//   authController.get(req, res);
-// });
-
-// router.get('/users', (req: Request, res: Response) => {
-//   authController.getAll(req, res);
-// });
-
 router.post('/register', (req: Request, res: Response) => {
   authController.register(req, res);
 });
@@ -19,11 +11,3 @@ router.post('/register', (req: Request, res: Response) => {
 router.post('/login', (req: Request, res: Response) => {
   authController.login(req, res);
 });
-
-// router.patch('/:username', (req: Request, res: Response) => {
-//   authController.update(req, res);
-// });
-
-// router.delete('/:username', (req: Request, res: Response) => {
-//   authController.delete(req, res);
-// });
