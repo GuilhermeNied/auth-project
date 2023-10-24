772da770a258c8c13af09c18046c228b57ee2a1b
Guilherme <guilhermenied01@gmail.com>
2023-10-24 07:35:22 -0300

    States to login and disabled button when username or password are empty

--- client/src/components/SubmitButton/index.tsx
+++ client/src/components/SubmitButton/index.tsx
@@ -2,11 +2,12 @@ import './styles.css'
 
 interface SubmitButtonProps {
   submitButtonText: string
+  disabled?: boolean
 }
 
-export function SubmitButton({ submitButtonText }: SubmitButtonProps) {
+export function SubmitButton({ submitButtonText, disabled }: SubmitButtonProps) {
   return (
-    <button className="submit-button" type="submit">
+    <button className="submit-button" type="submit" disabled={disabled}>
       {submitButtonText}
     </button>
   )
--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,27 +1,55 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Input } from "../../components/Input";
 import { RedirectPageLink } from "../../components/RedirectPageLink";
 import { SubmitButton } from "../../components/SubmitButton";
 import './styles.css'
 
 export function Login() {
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  function handleChangeInput(setValue: (event: string) => void, event: ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value)
+  }
+
+  const isUsernameAndPasswordEmpty: boolean = username.length === 0 || password.length === 0
+
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (!isUsernameAndPasswordEmpty) {
+      console.log(username, password)
+      setUsername('')
+      setPassword('')
+    }
+  }
+
   return (
     <div className="login-page-container">
-      <form className="login-form">
+      <form onSubmit={handleSubmit} className="login-form">
         <h2>Login</h2>
         <Input
           labelText="Username:"
           placeholder="Username"
-          type="text" />
+          type="text"
+          value={username}
+          onChange={(event) => handleChangeInput(setUsername, event)}
+        />
 
         <Input
           labelText="Password:"
           placeholder="Password"
-          type="password" />
+          type="password"
+          value={password}
+          onChange={(event) => handleChangeInput(setPassword, event)}
+        />
 
         <RedirectPageLink
           redirectTo='/register'
-          redirectPageLinkText="If you have not registered, register here" />
-        <SubmitButton submitButtonText="Login" />
+          redirectPageLinkText="If you have not registered, register here"
+        />
+        <SubmitButton submitButtonText="Login" disabled={isUsernameAndPasswordEmpty} />
       </form>
     </div>
   )
