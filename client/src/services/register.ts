558d89c5b16d0beb7d9405adf6c2163761697ae9
Guilherme <guilhermenied01@gmail.com>
2023-10-25 07:40:23 -0300

    Adjusting and removing log

--- client/src/services/register.ts
+++ client/src/services/register.ts
@@ -2,11 +2,9 @@ import { User } from '../@types/User'
 import { axiosInstance } from './axiosInstance'
 
 export async function register({ name, username, password }: User) {
-  const user = await axiosInstance.post('/user/register', {
+  await axiosInstance.post('/user/register', {
     name,
     username,
     password
   })
-
-  console.log(user)
 }
