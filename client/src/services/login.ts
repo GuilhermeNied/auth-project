6403b512b0ee89e2ecb9319d5c5636b2a9de8657
Guilherme <guilhermenied01@gmail.com>
2023-10-29 08:21:21 -0300

    Removing session storage

--- client/src/services/login.ts
+++ client/src/services/login.ts
@@ -1,11 +1,8 @@
 import { User } from '../@types/User'
-import { setInSessionStorage } from '../utils/sessionStorage'
 import { axiosInstance } from './axiosInstance'
 
 export async function login({ username, password }: User) {
   const user = await axiosInstance.post('/user/login', { username, password })
-  const { token, userId } = user.data
-  setInSessionStorage('user', { token, userId, username })
 
   return user
 }
--- client/src/utils/sessionStorage.ts
+++
@@ -1,9 +0,0 @@
-function setInSessionStorage(key: string, value: any) {
-  sessionStorage.setItem(key, JSON.stringify(value))
-}
-
-function removeInSessionStorage(key: string) {
-  sessionStorage.removeItem(key)
-}
-
-export { setInSessionStorage, removeInSessionStorage }
