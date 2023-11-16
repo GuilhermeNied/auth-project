709f73dfff1200992164594bd3ec55bf15c519ee
Guilherme <guilhermenied01@gmail.com>
2023-11-16 07:13:39 -0300

    Testing auth middleware

---
+++ server/src/__tests__/authMiddleware.test.ts
@@ -0,0 +1,26 @@
+import { authMiddleware } from '../middlewares/authMiddleware';
+import jwt from 'jsonwebtoken';
+
+describe('Auth middleware test', () => {
+  it('should next is called when authentication is successful', () => {
+    // GIVEN
+    const req: any = {
+      headers: {
+        authorization: 'Bearer authToken',
+      },
+    };
+    const res: any = {
+      sendStatus: jest.fn(),
+    };
+
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {});
+
+    const next = jest.fn();
+
+    // WHEN
+    authMiddleware(req, res, next);
+
+    // THEN
+    expect(next).toHaveBeenCalled();
+  });
+});
